0ee44171a1d2b17f40a7121bf54f681185ad9c63
Anthony Martino <anthonym225@gmail.com>
2019-03-09 20:19:34 -0500

    Practice in C++ with switch/case statements/user input

---
+++ C++ Weight on Planets/weight.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+  double eWeight;
+  int planet;
+
+  std::cout << "What is your weight on Earth?: ";
+  std::cin >> eWeight;
+
+  std::cout << "Select a planet: \n";
+  std::cout << " 1 : Venus \n 2 : Mars \n 3 : Jupiter \n 4 : Saturn \n 5 : Uranus \n";
+  std::cin >> planet;
+
+  switch(planet){
+      case 1 :
+      std::cout << "Weight : " << (eWeight * 0.78) << " lbs \n";
+      break;
+      case 2 :
+      std::cout << "Weight : " << (eWeight * 0.39) << " lbs \n";
+      break;
+     case 3 :
+      std::cout << "Weight : " << (eWeight * 2.65) << " lbs \n";
+      break;
+      case 4 :
+      std::cout << "Weight : " << (eWeight * 1.17) << " lbs \n";
+      break;
+      case 5 :
+      std::cout << "Weight : " << (eWeight * 1.05) << " lbs \n";
+      break;
+      default :
+      std::cout << "Invalid \n";
+      break;
+  }
+
+}
