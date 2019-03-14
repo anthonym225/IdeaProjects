1de59c29f1f45e09307a8e1d62e694019d843572
Anthony Martino <anthonym225@gmail.com>
2019-03-14 13:46:06 -0400

    Enum practice

---
+++ Person/src/person/Person.java
@@ -0,0 +1,23 @@
+package person;
+
+import static person.HairColor.*;
+
+public class Person {
+
+
+    HairColor hairColor = BLACK;
+
+    public Person() {
+
+    }
+
+    public static void main(String[] args) {
+        Person peterParker = new Person();
+        Person spiderMan = peterParker;
+
+        spiderMan.hairColor = PINK;
+
+        System.out.println("Hair color of Peter Parker: " + peterParker.hairColor);
+        System.out.println("Hair color of Spiderman: " + spiderMan.hairColor);
+    }
+}
