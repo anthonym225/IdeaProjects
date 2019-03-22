c67279ce8f5cee2028947e6a3d1214e9023e2301
Anthony Martino <anthonym225@gmail.com>
2019-03-22 10:06:22 -0400

    More testing / exception practice

--- BSTpractice/src/BinarySearchTree/Testers.java
+++ BSTpractice/src/BinarySearchTree/Testers.java
@@ -1,6 +1,23 @@
 package BinarySearchTree;
 
+import java.util.Random;
+
 public class Testers{
+
+    // random integers
+    public static int rndInt(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
+    }
+
+    // random binary search trees
+    public static Tree rndTree(int count) {
+        if (count == 0) {
+            return new EmptyBST();
+        } else {
+            return rndTree(count - 1).add(rndInt(0, 50));
+        }
+    }
 
     public static void checkIsEmpty(Tree t) throws Exception {
         //If the tree T is an instance of EmptyBST --> t.isEmpty --> true
@@ -15,5 +32,25 @@ public class Testers{
                 }
             }
         }
+    }
+
+    public static void checkAddMemberCardinality(Tree t, int x) throws Exception{
+        int nT = (t.add(x)).cardinality();
+        // 1. Either something was added --> and the cardinality increased by 1.
+        if (nT == (t.cardinality() + 1)) {
+            if (t.member(x)) {
+                throw new Exception("The cardinality increased by 1, but the thing that was added was already a member of the tree.");
+            }
+            // 2. OR the thing that was added was already there and there not really added so the cardinality stayed the same
+        } else if (nT == t.cardinality()){
+            if (!t.member(x)){
+                throw new Exception("The cardinality did not increase by 1, but we added a new thing");
+            }
+
+        } else {
+            throw new Exception("Something is wrong with our program");
+        }
+
+
     }
 }
